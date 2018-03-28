1dea5542ab16a7cde4b4311a0b9c5113c38a655f
valj10.com <valjeudy@gmail.com>
2018-03-28 07:52:38 -0400

    Added fixtures and leagues routes

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, json
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+# Assuming dbs is in your app root folder
+e = create_engine('sqlite:///salaries.db') 
+fixtures = create_engine('sqlite:///Fixtures.db')
+app = Flask(__name__)
+api = Api(app)
+
+class LeagueList(Resource):
+    def get(self):
+        conn = fixtures.connect()
+        query = conn.execute('select "Code" from Leagues')
+        resultset = {'Leagues': [i[0] for i in query.cursor.fetchall()]}
+        return resultset
+
+class Fixtures(Resource):
+    def get(self,leagueCode):
+        conn = fixtures.connect()
+        query = conn.execute("select Date, HomeTeam, AwayTeam From '%s'"%leagueCode)
+        return query.cursor.fetchall()
+
+# Routes
+api.add_resource(LeagueList,'/leagues')
+api.add_resource(Fixtures,'/fixture/<string:leagueCode>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
