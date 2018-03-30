8cc49442a2b416b887b7e0e5eadd9bfe2a2e84aa
valj10.com <valjeudy@gmail.com>
2018-03-30 19:35:53 -0400

    added more fixtures

--- app.py
+++ app.py
@@ -13,13 +13,30 @@ class LeagueList(Resource):
         conn = fixtures.connect()
         query = conn.execute('select "Code" from Leagues')
         resultset = {'Leagues': [i[0] for i in query.cursor.fetchall()]}
+        conn.close()
         return resultset
+
 
 class Fixtures(Resource):
     def get(self,leagueCode):
         conn = fixtures.connect()
-        query = conn.execute("select Date, HomeTeam, AwayTeam From '%s'"%leagueCode)
-        return query.cursor.fetchall()
+        query = conn.execute("select Date, HomeTeam, AwayTeam, FTHG, FTAG , HTHG, HTAG, HTR, FTR From '%s'"%leagueCode)
+        
+        rows =  ((query.cursor.fetchall()))
+        result =[]
+        for row in rows:
+            result.append({'Date':row[0],
+                           'HomeTeam':row[1],
+                           'AwayTeam':row[2],
+                           'Full Time Home Goals':row[3],
+                           'Full Time Away Team Goals':row[4],
+                           'Half Time Home Team Goals':row[5],
+                           'Half Time Away Team Goals' : row[6],
+                           'Half Time Result': row[7],
+                           'Full Time Result':row[8]
+                           })
+        conn.close()
+        return result
 
 # Routes
 api.add_resource(LeagueList,'/leagues')
